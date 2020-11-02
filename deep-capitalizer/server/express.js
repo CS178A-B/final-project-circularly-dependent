ed3127be4153c592340cc31d34dd699356abd7a2
Siena Ha <sha021@ucr.edu>
2020-11-01 20:26:29 -0800

    change directory

--- deep-capitalizer/server/express.js
+++ deep-capitalizer/server/express.js
@@ -9,11 +9,9 @@ const app = express();
 // Save the port of our sever into a constant 'PORT'
 // process.env.PORT checks our environment variables to see if we already have a PORT defined.
 // if not, then we use PORT 4000
-const PORT = process. env.PORT || 4000;
+const PORT = process. env.PORT || 3000;
 
 // the built-in listen method expects at least one argument : port number, 
 //                                                           () is a callback function, log to the console.
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
-
-
 
