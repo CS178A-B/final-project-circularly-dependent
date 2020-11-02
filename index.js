a6b68a49e4d33bff3005571a7242e9d9ddc4a557
Siena Ha <sha021@ucr.edu>
2020-11-01 17:05:02 -0800

    working on server

---
+++ index.js
@@ -0,0 +1,16 @@
+/* entry point fo rour application*/
+
+// Bringing in the express framework and sotre it in a constant 'express'
+const express = require("express");
+
+// Initializing the express framework and save it to another constant 'app'
+const app = express();
+
+// Save the port of our sever into a constant 'PORT'
+// process.env.PORT checks our environment variables to see if we already have a PORT defined.
+// if not, then we use PORT 4000
+const PORT = process. env.PORT || 4000;
+
+// the built-in listen method expects at least one argument : port number, 
+//                                                           () is a callback function, log to the console.
+app.listen(PORT, () => console.log('Server is running on port ${PORT}'));
