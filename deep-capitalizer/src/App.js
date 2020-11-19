3678a9b9abf4d566f4721a7760f975de13f18dbf
GrayGorilla <nbren004@ucr.edu>
2020-11-19 04:52:30 -0800

    Connected client to server

--- deep-capitalizer/src/App.js
+++ deep-capitalizer/src/App.js
@@ -1,30 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button'
-// import { NavLink } from 'react-router-dom'
+import { SERVER_PORT } from './globals';
+import { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import logo from './logo.svg';
 
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [serverMsg, setServerMsg] = useState(null);
+  const [error, setError] = useState(null);
+  const [clientMsg, setClientMsg]  = useState(['Click to poke Server'])
+
+
+  // Example: await timeout(1000);  // 1 second
+  /* const timeout = delay => {
+    return new Promise(res => setTimeout(res, delay));
+  }*/
+
+  // Handle poke
+  useEffect(() => {
+    if (count) {
+      console.log('Poke Sent >>')
+      fetch(`http://localhost:${SERVER_PORT}/testPage`)
+      .then(res => res.json())
+      .then(
+        resp => { setServerMsg(resp.message) }, 
+        err => { setError(err) })
+      if (!error) {
+        console.log('count:', count)
+        console.log('Poke Recieved <<');
+      } else { console.log('Error:', error) }
+    }
+  }, [count, error]);
+
+  useEffect(() => {
+    if (count) {
+      let cm = [`You poked Server ${count} time${count > 1 ? 's' : ''}`];
+      cm.push(serverMsg)
+      setClientMsg(cm)
+    }
+  }, [serverMsg, count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          HELLO THIS IS CIRCULARLY DEPENDENT - Hello from client
+          {clientMsg[0]}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-        <Button>
-          Hello World
+        <Button onClick={() => { setCount(count + 1) }} variant='contained'>
+          Poke it
         </Button>
+        <p>
+          {clientMsg[1]}
+        </p>
       </header>
     </div>
   );
+
+  
+  // if (!serverMsg) {
+  //   return (
+  //     <div className='App'>
+  //       <header className='App-header'>
+  //         <img src={logo} className='App-logo' alt='logo' />
+  //         <p>
+  //           Click to poke Server
+  //         </p>
+  //         <Button onClick={() => { testServer() }} variant='contained'>
+  //           Poke it
+  //         </Button>
+  //       </header>
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div className='App'>
+  //       <header className='App-header'>
+  //         <img src={logo} className='App-logo' alt='logo' />
+  //         <p>
+  //           You poked Server {count} times
+  //         </p>
+  //         <p>
+  //           Server says: {serverMsg}
+  //         </p>
+  //         <Button onClick={() => { testServer() }} variant='contained'>
+  //           Poke it
+  //         </Button>
+  //       </header>
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
--- server/express.js
+++ server/express.js
@@ -1,8 +1,9 @@
-/* entry point fo rour application*/
+/* entry point for our application*/
 
 // Bringing in the express framework and sotre it in a constant 'express'
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const cors = require('cors');
 
 // Initializing the express framework and save it to another constant 'app'
 const app = express();
@@ -15,6 +16,9 @@ const PORT = process. env.PORT || 4000;
 // the built-in listen method expects at least one argument : port number, 
 //                                                           () is a callback function, log to the console.
 
+// Removes CORS error
+app.use(cors());
+
 // Serves up static Client build (React App)
 app.use('/', express.static(path.join(__dirname, '../deep-capitalizer/build')));
 
@@ -25,6 +29,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 
 app.get('/testPage', (req, res) => {
-  res.status(200).json({ "message" : "> Poke recieved & returned <" })
-  console.log("Counter poke initiated")
+  res.status(200).json({ "message" : "> Counter poke from Server <" })
+  console.log('Counter poke initiated')
 });
