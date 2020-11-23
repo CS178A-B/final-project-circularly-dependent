6e35b6349075b1db2e51c648573f47654100830d
Siena Ha <sha021@ucr.edu>
2020-11-23 01:42:54 -0800

    getting rid of unused warnings

--- deep-capitalizer/src/components/Home.js
+++ deep-capitalizer/src/components/Home.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
-import { createBrowserHistory } from 'history';
 import { Link } from 'react-router-dom'
 
 const Home = () => {
