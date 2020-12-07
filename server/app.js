66c91a7be5cfa459e1a9f81ed107f1cca58ae105
Siena Ha <sha021@ucr.edu>
2020-12-06 23:18:19 -0800

    parsed data to front

--- server/app.js
+++ server/app.js
@@ -1,4 +1,15 @@
 var mysql = require('mysql');
+const csv = require('csv-parser')
+
+const fs = require('fs')
+const results = [];
+
+fs.createReadStream('RS-20170701-20190630.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+  });
 
 var con = mysql.createConnection({
     host: 'localhost',
@@ -6,13 +17,23 @@ var con = mysql.createConnection({
     password: 'CS178!CD!dc'
 });
 
-
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
   });
 
-con.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  let query = "INSERT INTO category (id, name, description, created_at) VALUES ?";
+  con.query(query, results, (error, response) => {
+    console.log(error || response);
+  });
+
+  con.query(query, function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
  });
+
+
+  // con.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+//  });
