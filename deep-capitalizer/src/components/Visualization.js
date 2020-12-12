62b054c96bd3df87ab24ccaea30401d3cf07eac0
GrayGorilla <nbren004@ucr.edu>
2020-12-11 22:16:49 -0800

    File restructuring

--- deep-capitalizer/src/components/Visualization.js
+++ deep-capitalizer/src/components/Visualization.js
@@ -1,13 +1,11 @@
 import './App.css';
-import logo from '../logo.svg';
 
 const Visualization = () => {
     return (
         <div className='App'>
           <header className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
             <p>
-              Graphs go here
+              - Graphs go here -
             </p>
           </header>
         </div>
--- deep-capitalizer/src/index.js
+++ deep-capitalizer/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Routes from './Routes';
+import Routes from './routes/Routes';
 
 ReactDOM.render(
   <React.StrictMode>
