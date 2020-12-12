7bc2270c3716c2a0003264d134e3d844091c08e2
Siena Ha <sha021@ucr.edu>
2020-12-12 02:07:05 -0800

    check if it's working outside of express

--- server/app.js
+++ server/app.js
@@ -1,15 +1,4 @@
 var mysql = require('mysql');
-const csv = require('csv-parser')
-
-const fs = require('fs')
-const results = [];
-
-fs.createReadStream('RS-20170701-20190630.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    console.log(results);
-  });
 
 var con = mysql.createConnection({
     host: 'localhost',
@@ -22,18 +11,19 @@ con.connect(function(err) {
     console.log("Connected!");
   });
 
-  let query = "INSERT INTO category (id, name, description, created_at) VALUES ?";
-  con.query(query, results, (error, response) => {
-    console.log(error || response);
-  });
+  // let query = "INSERT INTO category (id, name, description, created_at) VALUES ?";
+  // con.query(query, results, (error, response) => {
+  //   console.log(error || response);
+  // });
 
-  con.query(query, function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
- });
+// con.query(query, function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+//  });
 
 
   // con.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 //     if (error) throw error;
 //     console.log('The solution is: ', results[0].solution);
 //  });
+exports.results = results;
