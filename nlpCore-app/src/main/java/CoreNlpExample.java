bbe08e95a2e1db755723420e67293a95e4a22b9f
Jason Chan <13chanja@gmail.com>
2020-12-14 22:28:39 -0800

    added the rest of the csv entries as part of the output data

--- nlpCore-app/src/main/java/CoreNlpExample.java
+++ nlpCore-app/src/main/java/CoreNlpExample.java
@@ -21,8 +21,8 @@ public class CoreNlpExample {
 
     public static JSONObject getGrouping(String text){
         JSONObject group = new JSONObject();
-        group.put("Product", 0);
-        group.put("Descriptor", 0); //Do you want a json array???
+        group.put("PRODUCT_NAME", 0);
+        group.put("DESCRIPTOR", 0); //Do you want a json array???
         boolean consecutive = true;//capture consecutive nouns
         boolean noun_fill = false;
 
@@ -68,13 +68,13 @@ public class CoreNlpExample {
                             pos.matches("NNPS"))
                             && consecutive){   //capture consecutive nouns
                         //System.out.println();
-                        //group.put("Product", String.format("%s ", word));
-                        if(group.get("Product").equals(0)){
-                            group.put("Product",String.format("%s ", word));
+                        //group.put("PRODUCT_NAME", String.format("%s ", word));
+                        if(group.get("PRODUCT_NAME").equals(0)){
+                            group.put("PRODUCT_NAME",String.format("%s ", word));
                         }
                         else {
-                            group.put("Product",
-                                    group.get("Product").toString().concat(String.format("%s ", word)));
+                            group.put("PRODUCT_NAME",
+                                    group.get("PRODUCT_NAME").toString().concat(String.format("%s ", word)));
                         }
                         noun_fill = true;
                     }
@@ -83,12 +83,12 @@ public class CoreNlpExample {
                         if(noun_fill){
                             consecutive = false;
                         }
-                        if(group.get("Descriptor").equals(0)){
-                            group.put("Descriptor", String.format("%s ", word));
+                        if(group.get("DESCRIPTOR").equals(0)){
+                            group.put("DESCRIPTOR", String.format("%s ", word));
                         }
                         else{
-                            group.put("Descriptor",
-                                    group.get("Descriptor").toString().concat(String.format("%s ", word)));
+                            group.put("DESCRIPTOR",
+                                    group.get("DESCRIPTOR").toString().concat(String.format("%s ", word)));
                         }
                     }
                     else{
@@ -131,39 +131,53 @@ public class CoreNlpExample {
 
         read.useDelimiter(delimiter);
 
-        JSONArray product = new JSONArray();
+        JSONObject product;
         JSONObject root = new JSONObject();
         String text = "";
-        for(int i = 1; i <= 16; i++){
+        String description = "";
+
+        //10 wide skip the titles on the csv
+        for(int i = 0; i < 10; i++){
             text = read.next();
-        }
-        //System.out.println(text);//DEBUG
-        //product = getGrouping(text);
-        //System.out.println(product);
-        while(read.hasNext()){
             //System.out.println(text);//DEBUG
-            product.add(getGrouping(text));
+        }
+        String purchaseData[] =
+                {"PO_NO","ISSUE_DATE","REQUESTOR_DEPARTMENT", "DEPARTMENT_DESC","VENDOR_CODE",
+                        "VENDOR_NAME","ITEM_DESC","PO_QUANTITY","UNIT_PRICE","ITEM_TOTAL_AMOUNT"};
+
+        JSONArray purchases = new JSONArray();
+        JSONObject purchase;
+        //int entryCount = 0;//track entries in the data
+
+        while(read.hasNext()){
+            purchase = new JSONObject();
+
+            for(int i = 0; i < 10; i++){
+                purchase.put(purchaseData[i], 0);
+                if(read.hasNext()){
+                    text = read.next();
+                    //System.out.println(text);//DEBUG
+                    purchase.put(purchaseData[i], text);
+                    
+                    //description
+                    if(i == 6){
+                        //System.out.println(text);
+                        description = text;
+                    }
+                }
+            }
+            product = getGrouping(description);
+            purchase.put("PRODUCT", product);
+            purchases.add(purchase);
             //System.out.println(product.get("Product"));
             //System.out.println(product.get("Descriptor"));
             //System.out.println(product);
             //writer.println(product);
-            for(int i = 1; i <= 9; i++){
-                if(read.hasNext()){
-                    text = read.next();
-                }
-            }
         }
-        root.put("Purchases", product);
+        root.put("PURCHASES", purchases);
         writer.println(root.toJSONString());
 
         writer.close();
-        // read some text in the text variable
-//        String text = "Hello, my first name is Jason. Using my green compact car," +
-//                " I drove to the store yesterday and bought some paleo groceries." +
-//                " rubber gloves";
-        //System.out.print("]");
-
-
     }
 }
 
