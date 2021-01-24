457533588109fec593b9862618336e462c9fd974
sha021 <sha021@ucr.edu>
2021-01-24 14:15:29 -0800

    userstore set function

--- deep-capitalizer/src/components/UsterStore.js
+++ deep-capitalizer/src/components/UsterStore.js
@@ -3,11 +3,14 @@ import { extendObservable} from 'mobx';
 class UserStore {
   constructor() {
     extendObservable(this, {
-      loading: true,
-      isLoggedIn: false,
-      username: ''
+      isLoggedIn: false
     })
+  }
+  setLoggedIn(newValue) {
+    this.isLoggedIn = newValue; 
   }
 }
 
-export default new UserStore();
+let stat = new UserStore();
+
+export default stat;
