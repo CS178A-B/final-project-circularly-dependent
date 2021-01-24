96ded1855d4acd1a8a60276d6331d7573d1d38b5
sha021 <sha021@ucr.edu>
2021-01-24 03:21:54 -0800

    using useEffect attemp to click

--- deep-capitalizer/src/components/Login.js
+++ deep-capitalizer/src/components/Login.js
@@ -71,106 +71,208 @@ export default function Login() {
   const [attempted, setAttempted] = useState(false);
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(null);
+  const [ID, setID] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
+    console.log('useEffect')
     const signInAttempt = async () => {
       if (attempted) {
-        await fetch (`http://localhost:${SERVER_PORT}/signIn`, {
+        console.log(ID)
+        setAttempted(false);
+        let res = await fetch (`http://localhost:${SERVER_PORT}/signIn`, {
           method: 'POST',
           headers: {
             'Accept': 'application/json',
             'Content-Type' : 'application/json'
-          }
+          },
+          body: JSON.stringify({
+            username: ID,
+            password: password
+          })
         })
-        .then(res => res.json())
-        .then(
-          UserStore.loading = false,
-          UserStore.isLoggedIn = true,
-          UserStore.username = res.username,
-          resp => { setSucess(true) },
-          err => { setError(err) })
-        if (!error) {
-          console.log('successfuly logged in')
-        } 
-        else { 
-          console.log('Error:', error)
+
+        let result = await res.json();
+
+        if (result && result.success) {
+          UserStore.loading = false;
+          UserStore.isLoggedIn = true;
+          UserStore.username = result.username;
+        }
+
+        else {
           UserStore.loading = false;
           UserStore.isLoggedIn = false;
         }
+        // .then(result => res.json())
+        // .then(
+        //   UserStore.loading = false,
+        //   UserStore.isLoggedIn = true,
+        //   UserStore.username = res.username,
+        //   resp => { setSuccess(true) },
+        //   err => { setError(err) })
+        // if (!error) {
+        //   console.log('successfuly logged in')
+        // } 
+        // else { 
+        //   console.log('Error:', error)
+        //   UserStore.loading = false;
+        //   UserStore.isLoggedIn = false;
+        // }
       }
     }
     signInAttempt()
   }, [attempted, error]);
 
-
-  if (UserStore.isLoggedIn) {
-    return;
-  }
-  else {
-    return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            // onChange={e => setID(e.target.value)} 
+            value={ID} 
+            onChange={(e) => setID(e.target.value)}
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            // type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            // className={classes.submit}
+            onClick={() => {
+              setAttempted(true);
+            }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
             </Grid>
-          </form>
-        </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
-    );
-  }
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
+
+//   if (UserStore.isLoggedIn) {
+//     return;
+//   }
+//   else {
+//     return (
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         <div className={classes.paper}>
+//           <Avatar className={classes.avatar}>
+//             <LockOutlinedIcon />
+//           </Avatar>
+//           <Typography component="h1" variant="h5">
+//             Sign in
+//           </Typography>
+//           <form className={classes.form} noValidate>
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               required
+//               fullWidth
+//               id="email"
+//               label="Email Address"
+//               name="email"
+//               autoComplete="email"
+//               // onChange={e => setID(e.target.value)} 
+//               value={ID} 
+// 			        onChange={(e) => setID(e.target.value)}
+//               autoFocus
+//             />
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               required
+//               fullWidth
+//               name="password"
+//               label="Password"
+//               type="password"
+//               id="password"
+//               autoComplete="current-password"
+//             />
+//             <FormControlLabel
+//               control={<Checkbox value="remember" color="primary" />}
+//               label="Remember me"
+//             />
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               color="primary"
+//               // className={classes.submit}
+//               onClick={() => {
+//                 console.log('onClick')
+//               }}
+//             >
+//               Sign In
+//             </Button>
+//             <Grid container>
+//               <Grid item xs>
+//                 <Link href="#" variant="body2">
+//                   Forgot password?
+//                 </Link>
+//               </Grid>
+//               <Grid item>
+//                 <Link href="#" variant="body2">
+//                   {"Don't have an account? Sign Up"}
+//                 </Link>
+//               </Grid>
+//             </Grid>
+//           </form>
+//         </div>
+//         <Box mt={8}>
+//           <Copyright />
+//         </Box>
+//       </Container>
+//     );
+//   }
+// }
