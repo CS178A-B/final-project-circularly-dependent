cc0f8ba5ebba9712a68ddcce6ff7044721d508e9
sha021 <sha021@ucr.edu>
2021-01-24 14:15:53 -0800

    added loggout page

--- deep-capitalizer/src/routes/Routes.js
+++ deep-capitalizer/src/routes/Routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 
 import Home from '../components/Home';
@@ -6,21 +6,29 @@ import Visualization from '../components/Visualization';
 import ServerTest from '../components/ServerTest';
 import ButtonAppBar from '../components/shared-components/Navbar';
 import Login from '../components/Login'
+import Logout from '../components/Logout'
 import { createBrowserHistory } from 'history';
+import { UserContext } from '../globals';
 
 export const history = createBrowserHistory();
 
 const Routes = () => {
+  const [value, setValue] = useState(false);
+  const providerValue = useMemo(() => ({value, setValue}), [value, setValue]);
+
   return (
     <div className='App'>
       <Router history={history}>
+        <UserContext.Provider value={{value, setValue}}>
         <ButtonAppBar/>
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/login' exact component={Login} />
-          <Route path='/visualization' exact component={Visualization} />
-          <Route path='/server-test' exact component={ServerTest} />
+            <Route path='/' exact component={Home} />
+            <Route path='/login' exact component={Login} />
+            <Route path='/logout' exact component={Logout} />
+            <Route path='/visualization' exact component={Visualization} />
+            <Route path='/server-test' exact component={ServerTest} />
         </Switch>
+        </UserContext.Provider>
       </Router>
     </div>
   )
