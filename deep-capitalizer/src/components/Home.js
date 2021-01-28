9af66188fb10f21e9d4da437beec7fa4e6aad7ba
GrayGorilla <nbren004@ucr.edu>
2021-01-28 12:45:34 -0800

    Added background image to home page

--- deep-capitalizer/src/components/Home.js
+++ deep-capitalizer/src/components/Home.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
+const imageLink = 'https://wallpapercave.com/wp/wp4831635.jpg';
+
 const Home = () => {
   return (
-    <>
+    <div style={{ 
+      backgroundImage: `url(${imageLink})`,
+      width:'100%',
+      height: "100vh"
+    }}>
       <br />
       <Link to='/visualization' style={{ textDecoration: 'none'}}>
         <Button color="primary" variant='contained' style={{ margin: 50}}>
@@ -17,7 +23,7 @@ const Home = () => {
         </Button>
       </Link>    
       <br />
-    </>
+    </div>
   );
 }
 
