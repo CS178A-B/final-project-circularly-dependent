ccdeae57b8b5db4ca12b6cebde01b43172668ba5
Jason Chan <13chanja@gmail.com>
2021-01-28 00:49:27 -0800

    Update Readme
    also put old info in process_update.sh

--- nlpCore-app/process_update.sh
+++ nlpCore-app/process_update.sh
@@ -16,8 +16,39 @@ START_ENTRY="1"
 END_ENTRY="10"
 #DATA_COUNT="5"
 
+#mvn clean
+#mvn compile
+#mvn install
 mvn clean install
+
+# This is how we run a maven without arguments
+#mvn exec:java
+
+# Previous Execs may not work anymore
+#mvn exec:java -Dexec.main
+#mvn exec:java -Dexec.mainClass=CoreNlpExample.main
+
+# Adding arguments
+#mvn exec:java -Dexec.args="<Input file> <Output file> <start entry cell> <Last entry cell>"
 mvn exec:java -Dexec.args="${DATA_FILE} ${OUT_FILE} ${START_ENTRY} ${END_ENTRY}"
+
+#For example
+#Both should work
+#mvn exec:java -Dexec.args="../server/testdata outdata.txt"
+#mvn exec:java -Dexec.args="../server/testdata outdata.txt 1 5"
+
+#Or example without the last entry number
+#mvn exec:java -Dexec.args="testfile outfile.txt"
+
+
+
+#Otherwise Run in Intellij...
+# open Intellij
+# open project
+# navigate to the root of the maven project (ie. final-project-circularly-dependent/nlpCore-app/) and select the pom.xml file to open
+# A prompt should come up asking if you want to open as project or file, select open as project.
+# In Intellij make sure to open the "maven project window" and click on the circular arrows to create the target directory with all the jar files.
+# or you could "try" running the program and it may build it anyway.
 
 echo "New JSON file: $OUT_FILE"
 
