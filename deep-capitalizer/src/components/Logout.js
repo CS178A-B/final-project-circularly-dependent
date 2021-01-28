e5b611737690e55bf4da0dfc3ed9bdc9630edd6c
sha021 <sha021@ucr.edu>
2021-01-27 19:08:18 -0800

    Logout layout using deencrypt

--- deep-capitalizer/src/components/Logout.js
+++ deep-capitalizer/src/components/Logout.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom'
 import { useState, useEffect, useContext } from 'react';
-import { UserContext } from '../globals'
-import { set } from 'mobx';
+import Typography from '@material-ui/core/Typography';
 
+import { useDencrypt } from "use-dencrypt-effect";
+
+const textValues = ["See You Again", "Come Back Soon!", "Good Bye", "Until Next Time"]
 
 const Logout = () => {
-  const {value, setValue} = useContext(UserContext);
-  const waitForBar = async () => {
-    await setValue(false)
-    console.log('finished')
-  }
-  waitForBar()
+  const { result, dencrypt } = useDencrypt();
+
+  useEffect(() => {
+    let i = 0;
+    const action = setInterval(() => {
+      dencrypt (textValues[i]);
+      i = i === textValues.length - 1 ? 0 : i + 1;
+    }, 2000);
+
+    return () => clearInterval(action);
+  }, []);
+
   return (
-    <>  
-      <p>This is Logout</p>
-    </>
+    <div> 
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Typography 
+        align='center'  
+        style={{ wordWrap: "break-word" }} 
+        variant='h4'>
+          {result}
+      </Typography>
+    </div>
   );
 }
-
 
 export default Logout;
 
