2c82af024c54e9ca83712be8af14f5e96c443af7
sha021 <sha021@ucr.edu>
2021-01-27 19:07:51 -0800

    Welcome layout added

--- deep-capitalizer/src/components/Login.js
+++ deep-capitalizer/src/components/Login.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -14,8 +12,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { useState, useEffect, useContext, useMemo } from 'react';
 import { SERVER_PORT, timeout } from '../globals';
-import UserStore from './UsterStore'
 import { UserContext } from '../globals'
+import { createBrowserHistory } from 'history';
+import { useDencrypt } from "use-dencrypt-effect";
+
+export const history = createBrowserHistory({forceRefresh:true});
 
 function Copyright() {
   return (
@@ -59,10 +60,8 @@ export default function Login() {
   const {value, setValue} = useContext(UserContext);
 
   useEffect(() => {
-    console.log('useEffect')
     const signInAttempt = async () => {
       if (attempted) {
-        console.log(ID)
         setAttempted(false);
         let res = await fetch (`http://localhost:${SERVER_PORT}/signIn`, {
           method: 'POST',
@@ -79,94 +78,119 @@ export default function Login() {
         let result = await res.json();
         let success = result;
         
-        if (success) {
-          console.log(value)
-          setValue(true)
-          console.log(value)
-          console.log('successfully logged in')
+        try{
+          if (success) {
+            setValue(true)
+          } 
+          else {
+            console.log("here to change setvalue")
+            alert('username and password do not match')
+          }
         }
-
-        else {
-          UserStore.isLoggedIn = false;
-          console.log(value)
-
-          console.log('failed to log in')
-          alert('Email Address doesn\'t match with password')
-        }
+        catch(e) {} 
       }
     }
     signInAttempt()
-  }, [attempted, error, value]);
+  }, [attempted, error]);
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            value={ID} 
-            onChange={(e) => setID(e.target.value)}
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="current-password"
-          />
-          {/* <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          /> */}
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={() => {
-              setAttempted(true);
-            }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+  const textValues = ["You are signed in", "WELCOME"]
+  const { result, dencrypt } = useDencrypt();
+
+  useEffect(() => {
+    let i = 0;
+    const action = setInterval(() => {
+      dencrypt (textValues[i]);
+      i = i === textValues.length - 1 ? 0 : i + 1;
+    }, 2000);
+
+    return () => clearInterval(action);
+  }, []);
+  
+  
+  if (!value) {
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              value={ID} 
+              onChange={(e) => setID(e.target.value)}
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={() => {
+                setAttempted(true);
+              }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    );
+  }      
+  else {
+    return (
+      <div> 
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Typography 
+        align='center'  
+        style={{ wordWrap: "break-word" }} 
+        variant='h4'>
+          {result}
+      </Typography>
+    </div>
+    );
+  }
 }
+
