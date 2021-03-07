1b77ea0397e18db44cc2ec2eb07f7d6d27a6b69c
GrayGorilla <nbren004@ucr.edu>
2021-03-07 13:27:58 -0800

    added textfield attributes, minor code cleanup

--- deep-capitalizer/src/components/Signup.js
+++ deep-capitalizer/src/components/Signup.js
@@ -16,9 +16,9 @@ import ButtonAppBar from './shared-components/Navbar';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color='inherit' href='https://material-ui.com/'>
         Circularly Dependent
       </Link>{' '}
       {new Date().getFullYear()}
@@ -53,94 +53,95 @@ export default function SignUp() {
   return (
     <>
       <ButtonAppBar />
-      <Container component="main" maxWidth="xs">
+      <Container component='main' maxWidth='xs'>
         <CssBaseline />
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component='h1' variant='h5'>
             Sign up
           </Typography>
           <form className={classes.form} noValidate>
             <Grid container spacing={2}>
               {/* <Grid item xs={12} sm={6}>
                 <TextField
-                  autoComplete="fname"
-                  name="firstName"
-                  variant="outlined"
+                  autoComplete='fname'
+                  name='firstName'
+                  variant='outlined'
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
+                  id='firstName'
+                  label='First Name'
                   autoFocus
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
-                  variant="outlined"
+                  variant='outlined'
                   required
                   fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="lname"
+                  id='lastName'
+                  label='Last Name'
+                  name='lastName'
+                  autoComplete='lname'
                 />
               </Grid> */}
               <Grid item xs={12}>
                 <TextField
-                  variant="outlined"
+                  variant='outlined'
                   required
                   fullWidth
-                  name="city"
-                  label="City"
-                  type="city"
-                  id="city"
-                  autoComplete="city"
+                  id='city'
+                  name='city'
+                  type='text'
+                  label='City'
+                  autoComplete='city'
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                  variant="outlined"
+                  variant='outlined'
                   required
                   fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
+                  id='email'
+                  name='email'
+                  type='email'
+                  label='Email Address'
+                  autoComplete='email'
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                  variant="outlined"
+                  variant='outlined'
                   required
                   fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
+                  id='password'
+                  name='password'
+                  type='password'
+                  label='Password'
+                  autoComplete='current-password'
                 />
               </Grid>
               <Grid item xs={12}>
                 <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
+                  control={<Checkbox value='allowExtraEmails' color='primary' />}
+                  label='I want to receive inspiration, marketing promotions and updates via email.'
                 />
               </Grid>
             </Grid>
             <Button
-              type="submit"
+              type='submit'
               fullWidth
-              variant="contained"
-              color="primary"
+              variant='contained'
+              color='primary'
               className={classes.submit}
             >
               Sign Up
             </Button>
-            <Grid container justify="flex-end">
+            <Grid container justify='flex-end'>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href='#' variant='body2'>
                   Already have an account? Sign in
                 </Link>
               </Grid>
