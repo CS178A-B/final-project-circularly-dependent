0f756080d05e04bb9eb1aa0fb11211f88f9cd999
Jason Chan <13chanja@gmail.com>
2021-03-07 03:43:38 -0800

    listener working
    its not clean but it works

--- nlpCore-app/listener.sh
+++ nlpCore-app/listener.sh
@@ -1,20 +1,26 @@
 
-IN_FILE=../server/client-to-nlp/newData.json
-LOG_FILE=../server/process_log
+IN_FILE="../server/client-to-nlp/newData.json"
+OUT_FILE="../server/nlp-to-server/outdata.json"
+LOG_FILE="../server/process_log"
 
-#until $(curl --output IN_FILE --silent --head --fail http://myhost:myport); do
-#	printf '.'
-#	sleep 5
-#done
+START_ENTRY="1"
+END_ENTRY="10"
 
-inotifywait -m $IN_FILE -e attrib |
-	while read dir action file; do
-		CURRENT_TIME=$(date +%Y.%m.%d - %H:%M:%S)
-		echo "$IN_FILE was modified at $CURRENT_TIME"
-		echo "$IN_FILE was modified at $CURRENT_TIME" >> $LOG_FILE
-		echo "Executing process.sh"
-		#We cann add multi processing functionality here...
-		# using sh
-		./process_update.sh
-	done
 
+while [ 1 ]; do
+	echo "READY..."
+	inotifywait -m $IN_FILE -e attrib |
+		while true; do
+			break
+		done
+	CURRENT_TIME=$(date "+%Y.%m.%d-%H:%M:%S")
+	echo "$IN_FILE was modified at: $CURRENT_TIME"
+	echo "$IN_FILE was modified at: $CURRENT_TIME" >> $LOG_FILE
+	echo "Executing process.sh"
+	# We can add multi processing functionality here...
+	# sh or exec ? then exit or something idr
+	./process_update.sh
+	
+	#curl -v http://localhost:4000/test
+done
+	
