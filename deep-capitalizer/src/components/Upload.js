5bed7dc055ba3d73ee88d7fd9786167412f855c3
sha021 <sha021@ucr.edu>
2021-03-09 17:04:00 -0800

    button to green

--- deep-capitalizer/src/components/Upload.js
+++ deep-capitalizer/src/components/Upload.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Cards from './shared-components/Card';
 import Grid from '@material-ui/core/Grid';
 import ButtonAppBar from './shared-components/Navbar';
+import Button from '@material-ui/core/Button';
 
 
 const Upload = () => {
@@ -57,7 +58,6 @@ const Upload = () => {
     },
 
     label: {
-
       top: '30%',
       padding: '1rem'
     },
@@ -72,20 +72,29 @@ const Upload = () => {
       // marginRight: '-50%',
       // transform: 'translate(-50%, -50%)' ,
 
-      backgroundColor: '#363396',
+      backgroundColor: '#145e00',
+      // color: 'white',
+      // padding: '0.5rem',
+      // fontFamily: 'sans-serif',
+      // borderRadius: '0.3rem',
+      // cursor: 'pointer',
+      // marginTop: '1rem',
+      // borderStyle: 'none',
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.0)',
+
+      // background: 'linear-gradient(45deg, #177000 30%, #0b8a00 90%)',
+      borderRadius: 3,
+      border: 0,
       color: 'white',
-      padding: '0.5rem',
-      fontFamily: 'sans-serif',
-      borderRadius: '0.3rem',
-      cursor: 'pointer',
-      marginTop: '1rem',
-      borderStyle: 'none',
-      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.0)'
+      height: 48,
+      padding: '0 30px',
+      // boxShadow: '0 3px 5px 2px rgba(41, 84, 30, .3)',
     },
-    gridContainer: {
+      gridContainer: {
       paddingLeft: '2px',
       paddingRight: '2px',
-    }
+    },
+    
   }));
 
   const classes = useStyles();
