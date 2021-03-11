00a06dcb04514b5102c4d7cc00f08adfb420b920
GrayGorilla <nbren004@ucr.edu>
2021-03-11 12:06:06 -0800

    updated kibana dashboard (local instance)

--- deep-capitalizer/src/components/Dashboard.js
+++ deep-capitalizer/src/components/Dashboard.js
@@ -22,7 +22,7 @@ const Dashboard = () => {
   return (
     <div className={classes.root}>
       <ButtonAppBar />
-      <iframe className={classes.kibana} frameBorder='0' src="http://localhost:5601/goto/89ce5e2fd5370d0b335dd2d7ea07fdd7" />
+      <iframe className={classes.kibana} frameBorder='0' src="http://localhost:5601/goto/bd2a6d6717be3b32e53e1cfa83291c25" />
     </div>
   );
 }
