ccc67e91025957c400180e68465f2e02f3d878b0
sha021 <sha021@ucr.edu>
2021-03-10 19:43:48 -0800

    deleted unnessary variables

--- deep-capitalizer/src/components/Login.js
+++ deep-capitalizer/src/components/Login.js
@@ -89,7 +89,6 @@ const useStyles = makeStyles((theme) => ({
 export default function Login() {
   const classes = useStyles();
   const [attempted, setAttempted] = useState(false);
-  const [error, setError] = useState(null);
   const [ID, setID] = useState('');
   const [password, setPassword] = useState('');
   const {loggedIn, setLoggedIn} = useContext(UserContext);
