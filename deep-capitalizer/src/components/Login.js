65c82bd5b68d5b41ad3efa5d00d3f898a8c43fa9
GrayGorilla <nbren004@ucr.edu>
2021-03-11 07:43:01 -0800

    circularly dependent copyright linked to about us page

--- deep-capitalizer/src/components/Login.js
+++ deep-capitalizer/src/components/Login.js
@@ -18,7 +18,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="/aboutUs">
         Circularly Dependent
       </Link>{' '}
       {new Date().getFullYear()}
